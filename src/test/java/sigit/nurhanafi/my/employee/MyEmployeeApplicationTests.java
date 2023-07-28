55426a5dcc442d17b928793aa0b577cccc05406e
Sigit Nurhanafi <sigit.nurhanafi@gmail.com>
2023-07-28 15:21:04 +0700

    first init

---
+++ src/test/java/sigit/nurhanafi/my/employee/MyEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package sigit.nurhanafi.my.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
